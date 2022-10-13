d4356581b7327479d8c2e887539fa18d3251aedc
Kevin Le Guillou <1386917+kevinleguillou@users.noreply.github.com>
2022-10-13 21:10:00 +0200

    Update index.ts
    Allows handleJson to be used independently

--- src/openapi3/index.ts
+++ src/openapi3/index.ts
@@ -1,2 +1,3 @@
 export { default as parseOpenApi3Documentation } from "./parseOpenApi3Documentation.js";
 export * from "./parseOpenApi3Documentation.js";
+export { handleJson } from './handleJson.js';
